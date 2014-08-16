baae7507a7e588e69f60dce849dc79db2a5556cf
Gerard Flood <gerardflood@hotmail.com>
2014-08-16 23:01:35 +1000

    Initial commit

---
+++ Src/MVCMongo/Controllers/MongoController.cs
@@ -0,0 +1,126 @@
+﻿using MVCMongo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMongo.Controllers
+{
+    public class MongoController : Controller
+    {
+        MongoDBEntities db = new MongoDBEntities();
+        //
+        // GET: /Mongo/
+
+        public ActionResult Index()
+        {
+            var collection = from f in db.Departments
+                             select f;
+            return View(collection);
+        }
+
+        //
+        // GET: /Mongo/Details/5
+
+        public ActionResult Details(int id)
+        {
+            Department dep = (from f in db.Departments
+                              where f.DepartmentId == id
+                              select f).First();
+            return View(dep);
+        }
+
+        //
+        // GET: /Mongo/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Mongo/Create
+
+        [HttpPost]
+        public ActionResult Create(Department dept)
+        {
+            try
+            {
+                db.CreateDepartment(dept);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Mongo/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            List<Department> list = (from f in db.Departments
+                                     where f.DepartmentId == id
+                                     select f).ToList();
+            Department dept = new Department();
+            if (list.Count > 0) dept = list[0];
+            return View(dept);
+        }
+
+        //
+        // POST: /Mongo/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Department dept)
+        {
+            try
+            {
+                db.EditDepartment(dept);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Mongo/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Department dep = (from f in db.Departments
+                              where f.DepartmentId == id
+                              select f).First();
+            return View(dep);
+        }
+
+        //
+        // POST: /Mongo/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Department collection)
+        {
+            try
+            {
+                Department dep = (from f in db.Departments
+                                  where f.DepartmentId == id
+                                  select f).First();
+                db.DeleteDepartment(dep);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (db == null) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Src/MVCMongo/Models/MongoDB.cs
@@ -0,0 +1,82 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCMongo.Models
+{
+    public class Department
+    {
+        public ObjectId _id { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+ 
+
+    public class MongoDBEntities : DbContext
+    {
+        public MongoDBEntities() : base("name=MongoConnection") { }
+        static MongoServer server = MongoServer.Create(ConfigurationManager.ConnectionStrings["MongoConnection"].ConnectionString.ToString());
+        MongoDatabase database = server.GetDatabase("MVCMongo");
+
+        List<Department> department;
+        public List<Department> Departments
+        {
+            get
+            {
+                var collection = database.GetCollection<Department>("Department");
+                return collection.FindAllAs<Department>().ToList();
+            }
+            set { department = value; }
+        }
+        public void CreateDepartment(Department colleciton)
+        {
+            try
+            {
+                int Id = 0;
+                if (Departments.Count() > 0)
+                    Id = Departments.Max(x => x.DepartmentId);
+                Id += 1;
+                MongoCollection<Department> MCollection = database.GetCollection<Department>("Department");
+                BsonDocument doc = new BsonDocument { 
+                    {"DepartmentId",Id},
+                    {"DepartmentName",colleciton.DepartmentName}
+                };
+
+                IMongoQuery query = Query.EQ("DepartmentName", colleciton.DepartmentName);
+                var exists = MCollection.Find(query);
+                if (exists.ToList().Count == 0)
+                    MCollection.Insert(doc);
+
+            }
+            catch { }
+        }
+        public void EditDepartment(Department collection)
+        {
+            try
+            {
+                MongoCollection<Department> MCollection = database.GetCollection<Department>("Department");
+                IMongoQuery query = Query.EQ("DepartmentId", collection.DepartmentId);
+                IMongoUpdate update = MongoDB.Driver.Builders.Update.Set("DepartmentName", collection.DepartmentName);
+                MCollection.Update(query, update);
+            }
+            catch { }
+        }
+        public void DeleteDepartment( Department collection)
+        {
+            try
+            { 
+                MongoCollection<Department> MCollection = database.GetCollection<Department>("Department");
+                IMongoQuery query = Query.EQ("_id", collection._id);
+                MCollection.Remove(query);
+            }
+            catch { }
+        }
+
+    }
+}
